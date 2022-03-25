8a5dc287dba866e8dfb606b9ca619ba262d19968
loutre <loutre@courrier.dev>
2022-03-25 19:47:28 +0100

    cpp01

--- cpp_00/ex01/Contact.hpp
+++ cpp_00/ex01/Contact.hpp
@@ -2,7 +2,6 @@
 #ifndef __CONTACT_H__
 #define __CONTACT_H__
 
-
 #include <string>
 
 class Contact {
---
+++ cpp_01/ex00/Zombie.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __ZOMBIE_H__
+
+#include <string>
+
+class Zombie {
+	public:
+		void	announce( void );
+	private:
+		std::string	name;
+};
+
+#endif
