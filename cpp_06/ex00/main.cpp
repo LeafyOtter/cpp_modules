a260fa6180d41f064b97d96f5bad3510518546d1
loutre <loutre@courrier.dev>
2022-04-28 15:19:29 +0200

    cpp_06 ex00

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
 #include <istream>
 #include <sstream>
+#include <cctype>
 #include "type.hpp"
 
-void	printInt( std::stringstream str )
+using std::cout;
+using std::endl;
+
+void	printChar( char c )
 {
+	cout << "char: " << c << endl;
+	cout << "int: " << static_cast<int>(c) << endl;
+	cout << "float: " << static_cast<float>(c) << ".0f" << endl;
+	cout << "double: " << static_cast<double>(c) << ".0" << endl;
+}
+
+void	printInt( std::string str )
+{
+	int	i;
 	std::stringstream s(str);
-	int i;
 	s >> i;
 
-	log(i);
+	cout << "char: ";
+	if (isprint(i))
+		cout << static_cast<char>(i);
+	else
+		cout <<  "Non displayable";
+	cout << endl;
+	cout << "int: " << i << endl;
+	cout << "float: " << static_cast<float>(i) << ".0f" << endl;
+	cout << "double: " << static_cast<double>(i) << ".0" << endl;
+
+}
+
+void	printFloat( std::string str )
+{
+	float	f;
+	std::stringstream s(str);
+	s >> f;
+
+
+}
+
+void	printDouble( std::string str )
+{
+	double	d;
+	std::stringstream s(str);
+	s >> d;
 }
 
 int	main(int ac, char **av)
@@ -23,25 +60,22 @@ int	main(int ac, char **av)
 	type = getType(av[1]);
 	switch (type)
 	{
-		case t_char
+		case t_char :
 			printChar(av[1][0]);
 			break;
-		case t_int
+		case t_int :
+			printInt(av[1]);
 			break;
-		case t_float
+		case t_float :
+			printFloat(av[1]);
 			break;
-		case t_double
+		case t_double :
+			printDouble(av[1]);
 			break;
 		default:
+			log("Not a scalar type");
 			break;
 	}
-	// print int
-
-	// print char
-
-	// print double
-
-	// print float
 }
 
 /*
